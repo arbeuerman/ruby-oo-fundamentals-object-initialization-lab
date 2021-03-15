ce18ca27397031edde053fc0b1a53ba0013544fe
Alexandra <arbeuerman@gmail.com>
2021-03-15 17:46:03 -0500

    add initializers to classes

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+
+
+class Dog
+
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    end 
+
+    def name=(name)
+        @name = name
+    end 
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end 
+
+end 
+
+lassie = Dog.new("Lassie", "Collie")
+puts lassie.breed
+
+ronnie = Dog.new("Ronnie")
+puts ronnie.breed
